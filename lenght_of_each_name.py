07c1512121c3f3dca570c42e3f6bddeab5d03c9c
Dmytro Pogribnyy <lawukraine@gmail.com>
2017-12-12 16:54:47 +0200

    Create lenght_of_each_name.py

---
+++ lenght_of_each_name.py
@@ -0,0 +1,12 @@
+names = ['Ann', 'Fillip', 'Monika', 'Ross', 'Sam', 'Alf', 'Roy', 'Sasha', 'Leon']
+for i in names:
+    print i, len(i)
+Ann 3
+Fillip 6
+Monika 6
+Ross 4
+Sam 3
+Alf 3
+Roy 3
+Sasha 5
+Leon 4
