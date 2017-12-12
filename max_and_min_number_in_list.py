ce56d43d4d0685d80b8958ea7b39829f00abb092
Dmytro Pogribnyy <lawukraine@gmail.com>
2017-12-12 16:56:15 +0200

    Create max_and_min_number_in_list.py

---
+++ max_and_min_number_in_list.py
@@ -0,0 +1,16 @@
+numbers = [1, 33, 7, 18, 7, 9, 11, -5, 0, 12, 32]
+max_value = numbers[0]
+for i in numbers:
+      if i > max_value:
+      max_value = [i]
+      print "max number is", max_value
+max number is [33]
+
+
+#MIN NUMBER IN LIST
+
+min_value = numbers[8]
+for i in numbers:
+    if i < min_value:
+    print "min number is", i
+min number is -5
