0856fe8ee31d71cebab8e859914c4cf312a5fc67
Dmytro Pogribnyy <lawukraine@gmail.com>
2017-12-12 16:57:35 +0200

    Create print_index_of_names.py

---
+++ print_index_of_names.py
@@ -0,0 +1,14 @@
+names = ['Ann', 'Fillip', 'Monika', 'Ross', 'Sam', 'Alf', 'Roy', 'Sasha', 'Leon']
+index = 0
+for item in names:
+    print (index, item)
+    index +=1
+(0, 'Ann')
+(1, 'Fillip')
+(2, 'Monika')
+(3, 'Ross')
+(4, 'Sam')
+(5, 'Alf')
+(6, 'Roy')
+(7, 'Sasha')
+(8, 'Leon')
